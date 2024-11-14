85ede1128a4898a9062f3cd04168423d9097639f
Jalen Lee <90602982+li382112772@users.noreply.github.com>
2024-11-14 19:56:51 +0800

    Update database.go

--- config/database.go
+++ config/database.go
@@ -14,28 +14,35 @@ import (
 func Database() *sql.DB {
 	logger, _ := thoth.Init("log")
 
-	user, exist := os.LookupEnv("DB_USER")
+	user, exist := os.LookupEnv("MYSQL_USER")
 
 	if !exist {
-		logger.Log(errors.New("DB_USER not set in .env"))
-		log.Fatal("DB_USER not set in .env")
+		logger.Log(errors.New("MYSQL_USER not set in .env"))
+		log.Fatal("MYSQL_USER not set in .env")
 	}
 
-	pass, exist := os.LookupEnv("DB_PASS")
+	pass, exist := os.LookupEnv("MYSQL_PASSWORD")
 
 	if !exist {
-		logger.Log(errors.New("DB_PASS not set in .env"))
-		log.Fatal("DB_PASS not set in .env")
+		logger.Log(errors.New("MYSQL_PASSWORD not set in .env"))
+		log.Fatal("MYSQL_PASSWORD not set in .env")
 	}
 
-	host, exist := os.LookupEnv("DB_HOST")
+	host, exist := os.LookupEnv("MYSQL_HOST")
 
 	if !exist {
-		logger.Log(errors.New("DB_HOST not set in .env"))
-		log.Fatal("DB_HOST not set in .env")
+		logger.Log(errors.New("MYSQL_HOST not set in .env"))
+		log.Fatal("MYSQL_HOST not set in .env")
 	}
 
-	credentials := fmt.Sprintf("%s:%s@(%s:3306)/?charset=utf8&parseTime=True", user, pass, host)
+	port, exist := os.LookupEnv("MYSQL_PORT")
+
+	if !exist {
+		logger.Log(errors.New("MYSQL_PORT not set in .env"))
+		log.Fatal("MYSQL_PORT not set in .env")
+	}
+
+	credentials := fmt.Sprintf("%s:%s@(%s:%s)/?charset=utf8&parseTime=True", user, pass, host, port)
 
 	database, err := sql.Open("mysql", credentials)
 
